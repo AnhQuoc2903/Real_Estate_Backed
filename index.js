f000d595e5e760f490159b95df5adc3090642600
AnhQuoc2903 <133018919+AnhQuoc2903@users.noreply.github.com>
2026-02-06 14:54:15 +0700

    change server

--- index.js
+++ index.js
@@ -37,6 +37,10 @@ app.use("/api/uploads", uploadRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
+
 // Chỉ khởi động server khi đã kết nối DB thành công
 const startServer = async () => {
   try {
